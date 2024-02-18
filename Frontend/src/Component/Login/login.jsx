54e8064aaf19c59bdaf3a9c9d6c4556292988dec
unknown <you@example.com>
2024-02-18 17:44:40 +0530

    first commit

---
+++ Frontend/src/Component/Login/login.jsx
@@ -0,0 +1,33 @@
+import {useEffect} from "react";
+import './login.css';
+import farmer from '../../images/farmer2.jpg';
+import farmer2 from '../../images/wholesaler.jpg';
+import { Link } from 'react-router-dom';
+import Aos from "aos"; // Import CSS file for styling
+import 'aos/dist/aos.css';
+
+const Login = () => {
+  useEffect(()=>{
+    Aos.init({duration:2000})
+  }, [])
+
+    return (  
+    <div data-aos="fade-up" className="centered-container">
+      <div className='image'>
+      <Link to="/farmer">
+      <img className="centered-image" src={farmer} alt="Image 1"/>
+      <p className='image-name'>Farmer</p>
+      </Link>
+      </div>
+      
+      <div className='image'>
+        <Link to="/wholesaler">
+      <img className="centered-image" src={farmer2} alt="Image 2"/>
+      <p className='image-name'>Wholesaler</p>
+      </Link>
+      </div>
+    </div>
+
+    )
+}
+    export default Login;
